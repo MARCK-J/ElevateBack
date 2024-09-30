d1d7d98efa72d83ad73fa6ca9ee1006993eeec3c
MARCK-J <105019521+MARCK-J@users.noreply.github.com>
2024-09-29 21:18:32 -0400

    cambio error login

--- src/main/java/ucb/edu/bo/Elevate/DAO/TeacherDAO.java
+++ src/main/java/ucb/edu/bo/Elevate/DAO/TeacherDAO.java
@@ -3,9 +3,9 @@ package ucb.edu.bo.Elevate.DAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import ucb.edu.bo.Elevate.Entity.Teacher;
 
 import java.util.List;
-import ucb.edu.bo.Elevate.Entity.Teacher;
 
 @Repository
 public interface TeacherDAO extends JpaRepository<Teacher, Integer> {
