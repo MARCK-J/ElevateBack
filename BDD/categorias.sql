5b9a20e321db52b5d05b27a59abcc559c77a7885
Luis Huanca <luanhuancam@gmail.com>
2024-10-18 21:56:49 -0400

    modificacion de la creacion de BD

--- BDD/categorias.sql
+++ BDD/categorias.sql
@@ -1,6 +1,3 @@
-delete from category_course.
-
-ALTER SEQUENCE category_course_id_seq RESTART WITH 1;
 
 INSERT INTO Category_course (name_category) VALUES
 ('Programacion'),
@@ -9,4 +6,3 @@ INSERT INTO Category_course (name_category) VALUES
 ('Quimica'),
 ('Historia');
 
-select * from Category_course
